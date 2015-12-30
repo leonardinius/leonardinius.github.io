81b42ab0a540f2fc0f4092e306278728e346a8c0
Leonids Maslovs <leonids.maslovs@galeoconsulting.com>
2015-12-30 12:12:38 +0200

    Move to jekyll/docker for local runs

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#$FORCE_APK_INSTALL - Force install .apk 100% of the time.
+#$BUNDLE_CACHE - Cache and install to the vendor/bundle folder.
+#$POLLING - Force polling with --force_polling.
+#$VERBOSE -  Enable jekyll --verbose.
+
+
+docker run --rm \
+  --label=jekyll \
+  --volume=$(pwd):/srv/jekyll \
+  -it \
+  \
+  --env BUNDLE_CACHE=1 \
+  --env POLLING=1 \
+  --env VERBOSE=1 \
+  \
+  -p 127.0.0.1:4000:4000 \
+  \
+  jekyll/jekyll:pages
